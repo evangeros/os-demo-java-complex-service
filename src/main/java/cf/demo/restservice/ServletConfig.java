ee8bede7be8f999caca5597b269f6d448130903f
EvangelR <ronald.evangelista@transport.nsw.gov.au>
2017-08-24 16:09:42 +1000

    disable use of port environment

--- src/main/java/cf/demo/restservice/ServletConfig.java
+++ src/main/java/cf/demo/restservice/ServletConfig.java
@@ -7,19 +7,20 @@ import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletConta
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component
-public class ServletConfig implements EmbeddedServletContainerCustomizer {
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer container) {
-        container.setPort(Integer.parseInt(System.getenv("PORT")));
-    }
+//@Component
+public class ServletConfig {
+  //implements EmbeddedServletContainerCustomizer
+//    @Override
+//    public void customize(ConfigurableEmbeddedServletContainer container) {
+//        container.setPort(Integer.parseInt(System.getenv("PORT")));
+//    }
 
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
-        factory.setPort(Integer.parseInt(System.getenv("PORT")));
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
+//        factory.setPort(Integer.parseInt(System.getenv("PORT")));
 //        factory.setSessionTimeout(10, TimeUnit.MINUTES);
 //        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
-        return factory;
-    }
+//        return factory;
+//    }
 }
